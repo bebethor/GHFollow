aaaab85fb2e3b0da15870719efd003a1f1b52d57
Jose Alberto Ruíz-Carrillo González <jose.ruicarrillo@securitasdirect.es>
2024-08-14 12:28:02 +0200

    Your Message

---
+++ GHFollow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollow
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 14/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
